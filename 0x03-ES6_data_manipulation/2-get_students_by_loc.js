e1e0dc718c956c5d1ff3833a8df1cbf2bdf219b8
CyberGA <etomu.joshua@gmail.com>
2024-05-01 03:09:56 +0100

    fix

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,3 +2,5 @@ const getStudentsByLocation = (arr, city) => {
   const res = arr.filter((ele) => ele.location === city);
   return res;
 };
+
+export default getStudentsByLocation;
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,3 +2,5 @@ const getStudentIdsSum = (students) => {
   const idSum = students.reduce((acc, student) => acc + student.id, 0);
   return idSum;
 };
+
+export default getStudentIdsSum;
