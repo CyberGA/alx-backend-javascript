dc6a019ad511feab5f249f02e7388ce2070d307c
CyberGA <etomu.joshua@gmail.com>
2024-05-01 04:20:22 +0100

    fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    throw new Error("Position outside range");
+  if (position >= length || position < 0) {
+    throw Error("Position outside range");
   }
-  const buf = new DataView(new ArrayBuffer(length), 0, length);
-  buf.setInt8(position, value);
-  return buf;
+
+  const buff = new ArrayBuffer(length);
+  const arrInt8view = new DataView(buff);
+  arrInt8view.setInt8(position, value);
+  return arrInt8view;
 }
