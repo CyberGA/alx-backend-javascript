5d2bc92a0c065e863108c4024c5cffa095e9613c
CyberGA <etomu.joshua@gmail.com>
2024-05-01 04:19:20 +0100

    fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
-    return [];
-  }
-
-  const cityStudents = students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const item2 = newGrades.find(
-        (student2) => student.id === student2.studentId
-      );
-      return { ...student, grade: item2 ? item2.grade : "N/A" };
-    });
-  return cityStudents;
+  const studentsOfCity = students.filter((people) => people.location === city);
+  const studentsWithGrades = studentsOfCity.map((student) => {
+    const gradeObject = newGrades.find((gradesObject) => gradesObject.studentId === student.id);
+    const grade = gradeObject ? gradeObject.grade : 'N/A';
+    return { ...student, grade };
+  });
+  return studentsWithGrades;
 }
