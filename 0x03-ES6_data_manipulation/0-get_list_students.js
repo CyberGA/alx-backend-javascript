1ec8502043e8e7da83ab4fd2d79d3bf49fbaea0c
CyberGA <etomu.joshua@gmail.com>
2024-04-30 18:45:32 -0700

    fix: task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-const getListStudents = () => {
-  const student1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco'};
-  const student2 = {id: 2, firstName: 'James', location: 'Columbia'};
-  const student3 = {id: 5, firstName: 'Serena', location: 'San Francisco'};
-  return [student1, student2, student3];
-}
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 
 export default getListStudents;
