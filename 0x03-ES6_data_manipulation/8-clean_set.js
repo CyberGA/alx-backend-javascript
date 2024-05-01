1aa3cde85124e387fe905ea3ab13fb6fc0dba795
CyberGA <etomu.joshua@gmail.com>
2024-05-01 03:45:54 +0100

    task 8 to 11

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return "";
+  }
+  return [...set]
+    .filter((ele) => (ele !== undefined ? ele.startsWith(startString) : ""))
+    .map((ele) => (ele !== undefined ? ele.slice(startString.length) : ""))
+    .join("-");
+};
+
+export default cleanSet;
