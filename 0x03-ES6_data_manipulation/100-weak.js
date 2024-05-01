f3e2cc76f1924a111360eccc8eb78928faa314e0
CyberGA <etomu.joshua@gmail.com>
2024-05-01 04:18:02 +0100

    fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,27 +1,15 @@
-/**
- * A weak map of endpoints and the number of calls made.
- */
-export const weakMap = new WeakMap();
-
-/**
- * The maximum number of calls for an endpoint.
- */
-const MAX_ENDPOINT_CALLS = 5;
-
-/**
- * Tracks the number of calls made to an API's endpoint.
- * @param {{
- *   protocol: String,
- *   name: String,
- * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
-export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+const weakMap = new WeakMap();
+function queryAPI(endPoint) {
+  if (weakMap.has(endPoint)) {
+    weakMap.set(endPoint, weakMap.get(endPoint) + 1);
+  } else {
+    weakMap.set(endPoint, 1);
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error("Endpoint load is high");
+
+  if (weakMap.get(endPoint) >= 5) {
+    throw Error('Endpoint load is high');
   }
 }
+
+export { weakMap };
+export { queryAPI };
